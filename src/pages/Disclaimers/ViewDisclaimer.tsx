29889a558be88daa8679dd8d30b2b52807be287c
gus1000 <gramirez644@gmail.com>
2025-05-07 21:44:22 -0700

    fixed view page

--- src/pages/Disclaimers/ViewDisclaimer.tsx
+++ src/pages/Disclaimers/ViewDisclaimer.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useAuth } from "context/AuthContext";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ViewDisclaimer = () => {
   const { id } = useParams<{ id: string }>();
@@ -9,15 +10,33 @@ const ViewDisclaimer = () => {
 
   useEffect(() => {
     if (!id) return;
-    fetch(`http://localhost:3000/disclaimers/${id}`)
+
+    fetch(`http://localhost:3000/disclaimers/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+    })
       .then((res) => res.json())
       .then((data) => setDisclaimer(data))
-      .catch((err) => console.error("Failed to load disclaimer"));
+      .catch((err) => console.error("Failed to load disclaimer", err));
   }, [id]);
 
   return (
     <>
-      <h1> Disclaimer information</h1>
+      <h1>Disclaimer information</h1>
+
+      {disclaimer ? (
+        <div>
+          <div>{disclaimer.statement}</div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Link to={`/disclaimers/${id}/edit`}>Edit</Link>
+      <Link to="/dashboard">Dashboard</Link>
     </>
   );
 };
